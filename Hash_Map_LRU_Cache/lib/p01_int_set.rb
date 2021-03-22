5cbf95b9a1beaa8432382b139b318cdb7a599ba6
Joe <jkjoeeekim@live.com>
2021-03-22 08:53:31 -0700

    Add MaxIntSet class

--- Hash_Map_LRU_Cache/lib/p01_int_set.rb
+++ Hash_Map_LRU_Cache/lib/p01_int_set.rb
@@ -1,22 +1,32 @@
 class MaxIntSet
+  attr_reader :store
+
   def initialize(max)
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    validate!(num)
+    store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    store[num] = false
   end
 
   def include?(num)
+    store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    num.between?(0, (store.length-1))
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
